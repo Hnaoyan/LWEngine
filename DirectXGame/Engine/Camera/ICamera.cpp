7477eaff82877c6a17abeab5e28583eb8835c496
Hnaoyan <k022g0041@g.neec.ac.jp>
2024-05-23 11:33:14 +0900

    カメラクラスの作成

---
+++ DirectXGame/Engine/Camera/ICamera.cpp
@@ -0,0 +1,58 @@
+#include "ICamera.h"
+#include "../Base/DirectXCommon.h"
+#include "../Base/Utility/DxCreateLib.h"
+
+#include <cassert>
+
+void ICamera::Initialize()
+{
+	// バッファーの作成
+	this->CreateConstBuffer();
+	// マッピング
+	this->Map();
+	// 行列の更新
+	this->UpdateMatrix();
+}
+
+void ICamera::Map()
+{
+	HRESULT result;
+	result = constBuff_->Map(0, nullptr, (void**)&constMap_);
+	assert(SUCCEEDED(result));
+}
+
+void ICamera::CreateConstBuffer()
+{
+	// デバイス取得
+	ID3D12Device* device = DirectXCommon::GetInstance()->GetDevice();
+	// リソース作成
+	DxCreateLib::ResourceLib::CreateBufferResource(device, (sizeof(CBufferDataCamera) + 0xff) & ~0xff);
+}
+
+void ICamera::UpdateMatrix()
+{
+	// カメラ行列
+	Matrix4x4 cameraMatrix = Matrix4x4::MakeAffineMatrix(transform_.scale, transform_.rotate, transform_.translate);
+	// ビュー行列
+	viewMatrix_ = Matrix4x4::MakeInverse(cameraMatrix);
+	// プロジェクション行列
+	projectionMatrix_ = Matrix4x4::MakePerspectiveFovMatrix(fov_, aspectRatio_, nearZ, farZ);
+	// 角度
+	fovAngle_ = fov_ * (float)(std::numbers::pi / 180.0f);
+
+	// 定数バッファに送信
+	TransferMatrix();
+
+}
+
+void ICamera::TransferMatrix()
+{
+	// ビュー
+	constMap_->viewMatrix = viewMatrix_;
+	// プロジェクション
+	constMap_->projectionMatrix = projectionMatrix_;
+
+	Matrix4x4 translateMatrix = Matrix4x4::MakeTranslateMatrix(transform_.translate);
+	// ワールド座標
+	constMap_->worldPosition = { translateMatrix.m[3][0],translateMatrix.m[3][1] ,translateMatrix.m[3][2] };
+}
--- DirectXGame/Engine/Camera/ICamera.h
+++ DirectXGame/Engine/Camera/ICamera.h
@@ -1,21 +1,62 @@
 #pragma once
 #include "../3D/CBuffer.h"
+#include "../WindowAPI/WindowAPI.h"
 #include <wrl.h>
 #include <d3d12.h>
 #include <numbers>
 
 class ICamera
 {
-public:
-	virtual void Initialize();
-	virtual void Update();
 protected:
+	// 定数バッファ
+	Microsoft::WRL::ComPtr<ID3D12Resource> constBuff_;
+	// マッピング用
+	CBufferDataCamera* constMap_ = nullptr;
+
+public:	// 継承できるように
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	virtual void Initialize();
+	/// <summary>
+	/// 更新
+	/// </summary>
+	virtual void Update() {};
+
+protected: // 共通で使う関数
+	/// <summary>
+	/// バッファーにマッピング
+	/// </summary>
 	void Map();
-	void CreateCBuffer();
+	/// <summary>
+	/// バッファー作成
+	/// </summary>
+	void CreateConstBuffer();
+	/// <summary>
+	/// 行列更新
+	/// </summary>
 	void UpdateMatrix();
+	/// <summary>
+	/// マップに送る処理
+	/// </summary>
+	void TransferMatrix();
+
+protected:
+	// ビュー行列
+	Matrix4x4 viewMatrix_;
+	// プロジェクション行列
+	Matrix4x4 projectionMatrix_;
 
 public:
 	// 
 	Transform transform_ = {};
+	// 視野角の値
+	float fov_ = 45.0f;
+	// 視野角の角度
+	float fovAngle_ = fov_ * (float)(std::numbers::pi / 180.0);
+	// アスペクト比率
+	float aspectRatio_ = (float)WindowAPI::kWindowWidth / (float)WindowAPI::kWindowHeight;
 
+	float nearZ = 0.1f;
+	float farZ = 1000.0f;
 };
