0f9db9c1e8063441e48b4c455692d6035ba1e8a2
Hnaoyan <k022g0041@g.neec.ac.jp>
2024-06-12 19:58:33 +0900

    自力でローカル行列を求めるように変更

--- DirectXGame/Engine/3D/Loader.cpp
+++ DirectXGame/Engine/3D/Loader.cpp
@@ -337,16 +337,15 @@ ModelNode Loader::ReadNode(aiNode* node)
 	ModelNode modelNode;
 	aiMatrix4x4 aiLocalMatrix = node->mTransformation;
 
-	//aiVector3D scale, translate;
-	//aiQuaternion rotate;
+	aiVector3D scale, translate;
+	aiQuaternion rotate;
 
+	aiLocalMatrix.Decompose(scale, rotate, translate);
 
-	//aiLocalMatrix.Decompose(scale, rotate, translate);
-
-	//modelNode.transform.scale = { scale.x,scale.y,scale.z };
-	//modelNode.transform.rotate = { rotate.x,-rotate.y,-rotate.z,rotate.w };
-	//modelNode.transform.translate = { -translate.x,translate.y,translate.z };
-	//modelNode.localMatrix = Matrix4x4::MakeAffineMatrix(modelNode.transform.scale, modelNode.transform.rotate, modelNode.transform.translate);
+	modelNode.transform.scale = { scale.x,scale.y,scale.z };
+	modelNode.transform.rotate = { rotate.x,-rotate.y,-rotate.z,rotate.w };
+	modelNode.transform.translate = { -translate.x,translate.y,translate.z };
+	modelNode.localMatrix = Matrix4x4::MakeAffineMatrix(modelNode.transform.scale, modelNode.transform.rotate, modelNode.transform.translate);
 
 	//modelNode.offsetMatrix = Matrix4x4::MakeIdentity4x4();
 	//if (!boneOffsetMatrixes_.empty()) {
@@ -356,13 +355,6 @@ ModelNode Loader::ReadNode(aiNode* node)
 	//		}
 	//	}
 	//}
-	aiLocalMatrix.Transpose();							// 列ベクトル形式を行ベクトル形式に転置
-	//result.localMatrix.m[0][0] = aiLocalMatrix[0][0];	// 他の要素も同様に
-	for (uint32_t y = 0; y < 4; ++y) {
-		for (uint32_t x = 0; x < 4; ++x) {
-			modelNode.localMatrix.m[y][x] = aiLocalMatrix[y][x];
-		}
-	}
 
 	// ...
 	modelNode.name = node->mName.C_Str();
