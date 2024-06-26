f7c25f1f3e91fa772e8035aa6227921dae098489
Hnaoyan <k022g0041@g.neec.ac.jp>
2024-06-27 02:28:07 +0900

    コライダー関係の追加途中

---
+++ DirectXGame/Engine/Collision/Collider/ICollider.cpp
@@ -0,0 +1,6 @@
+#include "ICollider.h"
+
+void ICollider::Initialize(ColliderShape shape)
+{
+	colliderShape_ = shape;
+}
--- DirectXGame/Engine/Collision/Collider/ICollider.h
+++ DirectXGame/Engine/Collision/Collider/ICollider.h
@@ -1,7 +1,29 @@
 #pragma once
+#include "../../3D/WorldTransform.h"
+#include "ColliderShape.h"
 
 class ICollider
 {
 public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="shape"></param>
+	virtual void Initialize(ColliderShape shape);
+	/// <summary>
+	/// 更新
+	/// </summary>
+	virtual void Update() = 0;
 
+public:
+	// 衝突属性（自分）
+	uint32_t collisionAttribute_ = 0xffffffff;
+	// 衝突マスク
+	uint32_t collisionMask_ = 0xffffffff;
+
+public:
+	// ワールドトランスフォーム
+	WorldTransform worldTransform_{};
+	// 形状のポインタ
+	ColliderShape colliderShape_;
 };
