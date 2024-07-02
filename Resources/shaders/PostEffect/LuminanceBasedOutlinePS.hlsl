4de1eb82bc71dcc7a89e87dcff4c3e6aef0d6722
Hnaoyan <k022g0041@g.neec.ac.jp>
2024-07-02 15:48:22 +0900

    DissolveのPS内処理の書き込み完了 バッファーから渡す構造が未完成

--- Resources/shaders/PostEffect/LuminanceBasedOutlinePS.hlsl
+++ Resources/shaders/PostEffect/LuminanceBasedOutlinePS.hlsl
@@ -31,6 +31,25 @@ static const float32_t2 kIndex5x5[5][5] =
     { { -2.0f, 2.0f }, { -1.0f, 2.0f }, { 0.0f, 2.0f }, { 1.0f, 2.0f }, { 2.0f, 2.0f } },
 };
 
+static const float32_t kPrewittHorizontalKernel[3][3] =
+{
+    { -1.0f / 6.0f, 0.0f, 1.0f / 6.0f },
+    { -1.0f / 6.0f, 0.0f, 1.0f / 6.0f },
+    { -1.0f / 6.0f, 0.0f, 1.0f / 6.0f },
+};
+
+static const float32_t kPrewittVerticlaKernel[3][3] =
+{
+    { -1.0f / 6.0f, -1.0f / 6.0f, -1.0f / 6.0f },
+    { 0.0f, 0.0f, 0.0f },
+    { 1.0f / 6.0f, 1.0f / 6.0f, 1.0f / 6.0f },
+};
+
+float32_t Luminance(float32_t3 v)
+{
+    return dot(v, float32_t3(0.2125f, 0.7154f, 0.0721f));
+}
+
 struct PixelShaderOutput
 {
     float32_t4 color : SV_TARGET0;
@@ -46,7 +65,8 @@ PixelShaderOutput main(VertexShaderOutput input)
     output.color.rgb = float32_t3(0.0f, 0.0f, 0.0f);
     output.color.a = 1.0f;
     
-    int32_t kernelSize = 5;
+    float32_t2 difference = float32_t2(0.0f, 0.0f); // 縦横それぞれの畳み込みの値
+    int32_t kernelSize = 3;
     
     float kernelSizeSquared = 1.0f / (kernelSize * kernelSize);
     
@@ -55,14 +75,24 @@ PixelShaderOutput main(VertexShaderOutput input)
         for (int32_t y = 0; y < kernelSize; ++y)
         {
             // 3. 現在のtexcoordを算出
-            float32_t2 texcoord = input.texcoord + kIndex5x5[x][y] * uvStepSize;
+            float32_t2 texcoord = input.texcoord + kIndex3x3[x][y] * uvStepSize;
             // 4. 色に1/9掛けて足す
             float32_t3 fetchColor = gTexture.Sample(gSampler, texcoord).rgb;
             
-            // 計算
-            output.color.rgb += fetchColor * kernelSizeSquared;
+            float32_t luminance = Luminance(fetchColor);
+            difference.x += luminance * kPrewittHorizontalKernel[x][y];
+            difference.y += luminance * kPrewittVerticlaKernel[x][y];
         }
     }
     
+    // 変化の長さをウェイトとして合成。ウェイトの決定方法も色々と考えられる。
+    float32_t weight = length(difference);
+    
+    float32_t weightRatio = 6.0f;
+    
+    weight = saturate(weight * weightRatio);
+    
+    output.color.rgb = (1.0f - weight) * gTexture.Sample(gSampler, input.texcoord).rgb;
+     
     return output;
 }
