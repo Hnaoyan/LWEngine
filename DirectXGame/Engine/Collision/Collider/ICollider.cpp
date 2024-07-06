308058a58d24d238b6a77f356f71e43aae0a2936
Hnaoyan <k022g0041@g.neec.ac.jp>
2024-07-06 16:31:59 +0900

    コライダーの内部処理変更

--- DirectXGame/Engine/Collision/Collider/ICollider.cpp
+++ DirectXGame/Engine/Collision/Collider/ICollider.cpp
@@ -4,3 +4,12 @@ void ICollider::Initialize(ColliderObject object)
 {
 	objectRegistry_ = object;
 }
+
+void ICollider::Update(const Vector3& worldPosition)
+{
+	// 座標更新
+	worldTransform_.transform_.translate = worldPosition;
+	// 更新
+	worldTransform_.UpdateMatrix();
+
+}
