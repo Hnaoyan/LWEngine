a53d0317e784e527b4f4b1a88eee3f548d870117
Hnaoyan <k022g0041@g.neec.ac.jp>
2024-07-06 17:32:56 +0900

    地形クラス作成

---
+++ DirectXGame/Application/GameObject/Terrain/Terrain.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "../IGameObject.h"
+
+class Terrain : public IGameObject
+{
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="model"></param>
+	void Initialize(Model* model) override;
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update() override;
+	/// <summary>
+	/// 描画
+	/// </summary>
+	/// <param name="desc"></param>
+	void Draw(ModelDrawDesc desc) override;
+	/// <summary>
+	/// ImGui描画
+	/// </summary>
+	/// <param name="name"></param>
+	void ImGuiDraw() override;
+	/// <summary>
+	/// コールバック関数
+	/// </summary>
+	/// <param name="object"></param>
+	void OnCollision(ColliderObject object);
+
+};
