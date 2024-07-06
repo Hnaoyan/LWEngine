6de6e9a07fb7aa0761303fb791962c9d1e113be5
Hnaoyan <k022g0041@g.neec.ac.jp>
2024-07-06 18:54:03 +0900

    インスタンシングユニット用、親クラス80%・子クラス0%の進捗

--- DirectXGame/Engine/3D/Instancing/InstancedGroup.h
+++ DirectXGame/Engine/3D/Instancing/InstancedGroup.h
@@ -1,7 +1,51 @@
 #pragma once
+#include <vector>
+#include <memory>
+#include "../Drawer/Model.h"
+#include "InstancingData.h"
+#include "../../Base/Utility/DxCreateLib.h"
+
+class InstancedUnit;
 
 class InstancedGroup
 {
 public:
+	// 最大数
+	static const uint32_t kNumInstanceMax = 256;
+
+private:
+	Microsoft::WRL::ComPtr<ID3D12Resource> groupResources;
+	CBufferInstancedUnit* unitDataMap_ = nullptr;
+
+	std::pair<D3D12_CPU_DESCRIPTOR_HANDLE, D3D12_GPU_DESCRIPTOR_HANDLE> srvHandles_;
+	uint32_t srvIndex;
+
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="model"></param>
+	virtual void Initialize(Model* model);
+	/// <summary>
+	/// 更新
+	/// </summary>
+	virtual void Update();
+	/// <summary>
+	/// 描画
+	/// </summary>
+	/// <param name="desc"></param>
+	virtual void Draw(ModelDrawDesc desc);
+
+private:
+	/// <summary>
+	/// インスタンシングのSRV確保など
+	/// </summary>
+	void CreateData();
+
+private:
+	// ユニットリスト
+	std::vector<std::unique_ptr<InstancedUnit>> units_;
+	// モデル
+	Model* model_ = nullptr;
 
 };
--- DirectXGame/Engine/3D/Instancing/InstancedUnit.h
+++ DirectXGame/Engine/3D/Instancing/InstancedUnit.h
@@ -1,7 +1,10 @@
 #pragma once
+#include "../CBuffer.h"
 
-class InstancedUint
+class InstancedUnit
 {
 public:
 
+
+	EulerTransform eulerTransform_;
 };
