30739bc6cf77cefe88dcc5e0207be04c9c0de053
Hnaoyan <k022g0041@g.neec.ac.jp>
2024-08-27 07:07:09 +0900

    Sprite用のマネージャー作成

---
+++ DirectXGame/Engine/2D/SpriteManager.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Engine/Utility/Singleton.h"
+#include "Engine/2D/Drawer/Sprite.h"
+
+#include <unordered_map>
+#include <string>
+
+class SpriteManager : public Singleton<SpriteManager>
+{
+private:
+	// コンテナ
+	static std::unordered_map<std::string, std::unique_ptr<Sprite>> sSprites;
+
+public:
+	/// <summary>
+	/// スプライトの読み込み
+	/// </summary>
+	/// <param name="tag"></param>
+	/// <param name="filePath"></param>
+	static void LoadSprite(const std::string& tag, uint32_t texture);
+	/// <summary>
+	/// スプライトの取得
+	/// </summary>
+	/// <param name="tag"></param>
+	/// <returns></returns>
+	static Sprite* GetSprite(const std::string& tag);
+};
