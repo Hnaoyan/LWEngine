1256dc18a80e697f701b467efc71269a2f9bffb2
Hnaoyan <k022g0041@g.neec.ac.jp>
2024-10-24 11:58:15 +0900

    破壊されたエフェクトの板ポリクラス途中

---
+++ DirectXGame/Application/GameObject/Particle/User/Bullet/Break/BulletBreakEffect.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "../BulletParticleComponent.h"
+#include "Engine/3D/Instancing/InstancedUnit.h"
+
+namespace BulletParticle
+{
+	class BreakEffect : public Component, public InstancedUnit
+	{
+	public:
+		void Initialize() override;
+		void Update() override;
+
+
+	};
+
+}
