1f6906db33a1cc0348430eafec27e4e75396bb17
Hnaoyan <k022g0041@g.neec.ac.jp>
2024-11-04 20:36:20 +0900

    ゲームとしてクリアなどのシステムをマネージャでも対応できるように

--- DirectXGame/Application/Scene/GameScene.cpp
+++ DirectXGame/Application/Scene/GameScene.cpp
@@ -62,25 +62,15 @@ void GameScene::Update()
 	//	sceneManager_->ChangeScene("TITLE");
 	//}
 #endif // _DEBUG
-	//// 死亡チェック
-	//if (player_->IsDead() && !isGameOver_) {
-	//	backTitleTimer_.Start(120.0f);
-	//	isGameOver_ = true;
-	//}
-	//// シーン変更の処理
-	//if (isGameOver_) {
-	//	backTitleTimer_.Update();
-	//	if (!backTitleTimer_.IsActive()) {
-	//		sceneManager_->ChangeScene("TITLE");
-	//	}
-	//	return;
-	//}
-	//if (clearText_.isClear) {
-	//	clearText_.transitionTimer.Update();
-	//	if (clearText_.transitionTimer.IsEnd()) {
-	//		sceneManager_->ChangeScene("TITLE");
-	//	}
-	//}
+
+	if (gameObjectManager_->IsGameOver() || gameObjectManager_->IsGameClear()) {
+		sceneManager_->ChangeScene("TITLE");
+		return;
+	}
+	if (gameObjectManager_->IsGameClear()) {
+		sceneManager_->ChangeScene("TITLE");
+		return;
+	}
 
 	// ライトの更新
 	LightingUpdate();
@@ -148,10 +138,10 @@ void GameScene::UIDraw()
 	// UI全般
 	uiManager_->Draw();
 
-	if (clearText_.isClear) {
+	if (gameObjectManager_->IsUIGameClear()) {
 		clearText_.clearText->Draw();
 	}
-	if (isGameOver_) {
+	if (gameObjectManager_->IsUIGameOver()) {
 		SpriteManager::GetSprite("GameOverUI")->SetPosition({ 1280.0f / 2.0f,720.0f / 2.0f });
 		SpriteManager::GetSprite("GameOverUI")->Draw();
 	}
