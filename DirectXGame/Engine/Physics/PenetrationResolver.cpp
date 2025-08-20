1781031e3f081018c07627842c0963224c8effbe
Hnaoyan <k022g0041@g.neec.ac.jp>
2025-08-20 14:12:30 +0900

    押し出し

--- DirectXGame/Engine/Physics/PenetrationResolver.cpp
+++ DirectXGame/Engine/Physics/PenetrationResolver.cpp
@@ -2,9 +2,6 @@
 
 Vector3 PenetrationResolver::ExtrusionCalculation(AABB mover, AABB obstacle)
 {
-    Vector3 max = mover.max_ - obstacle.max_;
-    Vector3 min = mover.min_ - obstacle.min_;
-
     float overlapX1 = mover.max_.x - obstacle.min_.x;  // 右から
     float overlapX2 = mover.min_.x - obstacle.max_.x;  // 左から
     float overlapX = (overlapX1 < overlapX2) ? overlapX1 : -overlapX2;
@@ -31,4 +28,27 @@ Vector3 PenetrationResolver::ExtrusionCalculation(AABB mover, AABB obstacle)
     else {
         return Vector3(0.0f, 0.0f, overlapZ);
     }
+}
+
+Vector2 PenetrationResolver::Extrusion2DCalculation(Rectangle2D mover, Rectangle2D obstacle)
+{
+    float overlapX1 = mover.max_.x - obstacle.min_.x;  // 右から
+    float overlapX2 = mover.min_.x - obstacle.max_.x;  // 左から
+    float overlapX = (overlapX1 < overlapX2) ? overlapX1 : -overlapX2;
+
+    float overlapY1 = mover.max_.y - obstacle.min_.y;  // 上から
+    float overlapY2 = mover.min_.y - obstacle.max_.y;  // 下から
+    float overlapY = (overlapY1 < overlapY2) ? overlapY1 : -overlapY2;
+
+    // 最小の押し出し軸を選択
+    float absX = std::abs(overlapX);
+    float absY = std::abs(overlapY);
+
+    if (absX <= absY) {
+        return Vector2(overlapX, 0.0f);
+    }
+    else if (absY <= absX) {
+        return Vector2(0.0f, overlapY);
+    }
+    return Vector2(0.0f, 0.0f);
 }
