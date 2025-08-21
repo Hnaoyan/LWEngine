4daa9a60ec7dcf4e27ba90f4c4a018988ff58fc5
Hnaoyan <k022g0041@g.neec.ac.jp>
2025-08-21 19:04:39 +0900

    プレイヤー作成開始

---
+++ DirectXGame/Application/NetWorkGame/Player/Player.cpp
@@ -0,0 +1,21 @@
+#include "Player.h"
+
+void Player::Initialize(Model* model)
+{
+	IGameObject2D::Initialize(model);
+}
+
+void Player::Update()
+{
+
+}
+
+void Player::ImGuiDraw()
+{
+
+}
+
+void Player::OnCollision([[maybe_unused]] ColliderObject target)
+{
+
+}
--- DirectXGame/Application/NetWorkGame/Player/Player.h
+++ DirectXGame/Application/NetWorkGame/Player/Player.h
@@ -3,6 +3,12 @@
 
 class Player : public IGameObject2D
 {
+public:
+	void Initialize(Model* model) override;
+	void Update() override;
+	void ImGuiDraw() override;
+	void OnCollision([[maybe_unused]] ColliderObject target) override;
 
-
+public:
+	WorldTransform* GetWorldTransform() { return &worldTransform_; }
 };
