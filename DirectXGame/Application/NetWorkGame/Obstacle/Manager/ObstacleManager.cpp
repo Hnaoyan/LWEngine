89412af5082411f5b81efad1bbafa65f59594b55
Hnaoyan <k022g0041@g.neec.ac.jp>
2025-08-24 11:18:49 +0900

    途中

--- DirectXGame/Application/NetWorkGame/Obstacle/Manager/ObstacleManager.cpp
+++ DirectXGame/Application/NetWorkGame/Obstacle/Manager/ObstacleManager.cpp
@@ -1,1 +1,24 @@
 #include "ObstacleManager.h"
+#include "../Obstacle.h"
+
+void ObstacleManager::Initialize()
+{
+}
+
+void ObstacleManager::Update()
+{
+	for (std::list<std::unique_ptr<Obstacle>>::iterator it = obstacles_.begin();
+		it != obstacles_.end(); ++it)
+	{
+		(*it)->Update();
+	}
+}
+
+void ObstacleManager::Draw(ModelDrawDesc desc)
+{
+	for (std::list<std::unique_ptr<Obstacle>>::iterator it = obstacles_.begin();
+		it != obstacles_.end(); ++it)
+	{
+		(*it)->Draw(desc);
+	}
+}
--- DirectXGame/Application/NetWorkGame/Obstacle/Manager/ObstacleManager.h
+++ DirectXGame/Application/NetWorkGame/Obstacle/Manager/ObstacleManager.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Engine/3D/ModelUtility/ModelRenderer.h"
 #include <list>
 #include <memory>
 
@@ -9,8 +10,8 @@ class ObstacleManager
 public:
 	void Initialize();
 	void Update();
-	void Draw();
-
+	void Draw(ModelDrawDesc desc);
+	void AddObstacle(const Vector2& position, const Vector2& size);
 private:
 	// 障害物リスト
 	std::list<std::unique_ptr<Obstacle>> obstacles_;
